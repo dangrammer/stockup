b838169a239125bc30be7fc599e2fa4e62d415ed
dangrammer <danieljromans@gmail.com>
2020-02-15 14:59:31 -0500

    added user model validations, need to be tested

--- stockup-api/app/models/user.rb
+++ stockup-api/app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_secure_password
   has_many :stocks, dependent: :destroy
   has_many :transactions, dependent: :destroy
+
+  # need to be checked
+  validates :username, :password, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :username, length: {in: 2..20}
+  validates :password, length: {minimum: 8}
 end
